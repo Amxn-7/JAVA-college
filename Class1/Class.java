ac5a72baa62a22d7f1cd67ef9608f5b3eb1f20ce
Aman Singh <amxnsingh07@gmail.com>
2025-09-08 10:10:00 +0530

    add classwork

---
+++ Class1/Class.java
@@ -0,0 +1,20 @@
+//08-09-2025
+package Class1;
+
+public class Class {
+    public static void main(String[] args) {
+        int num1 =  Integer.parseInt(args[0]);
+        int num2 =  Integer.parseInt(args[1]);
+        float num3 = Float.parseFloat(args[0]);
+        float num4 = Float.parseFloat(args[1]);
+        System.out.println(num3+num4);
+        System.out.println(num1+num2);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+        System.out.println("Hello, World!");
+    }
+}
+//1. methods can not exist independentaly
+//2. all classes should be written in folders(packages)
+// create a test project
