6afbcaa4535fcee87ec3fd8e5c76cf243ca61208
Aman Singh <amxnsingh07@gmail.com>
2025-10-30 23:36:57 +0530

    commit changes

---
+++ Class11/Main.java
@@ -0,0 +1,12 @@
+package Class11;
+
+public class Main {
+    if(head==null || head.next==null) return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+}
